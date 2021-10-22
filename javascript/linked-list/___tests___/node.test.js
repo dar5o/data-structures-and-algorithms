e3a663092625f011ec6b905d2544865c98c417d9
dar5o <87797682+dar5o@users.noreply.github.com>
2021-10-22 15:51:03 -0700

    Update node.test.js

--- javascript/linked-list/___tests___/node.test.js
+++ javascript/linked-list/___tests___/node.test.js
@@ -4,7 +4,7 @@ const Node = require('../node');
 
 describe('Given Node', () => {
   describe('When constructing', () => {
-    it('Then should create valid node', () => {
+    it('should create valid node', () => {
       let value = 'Test Node';
       let node = new Node(value);
 
