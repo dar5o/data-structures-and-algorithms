5117a62af4070bd52b52724ba3afc67a31a51400
dar5o <87797682+dar5o@users.noreply.github.com>
2022-01-16 14:49:22 -0800

    Update linked-list.js

--- javascript/linked-list/linked-list.js
+++ javascript/linked-list/linked-list.js
@@ -7,31 +7,29 @@ class LinkedList {
     this.head = null;
   }
 
-  // Inserts a node at the start of a linked list
+
   insert(value) {
-    // IF the list is empty create it ELSE prepend to first node
+ 
     this.head === null
       ? this.head = new Node(value) 
       : this.head = new Node(value, this.head);
     return;
   }
 
-  // Check if a node value is in a linked list
+
   includes(value) {
     let current = this.head;
     while (current) {
       if(current.value === value) {
-        // Return true if object is found and exit the while loop
+
         return true;
       }
       current = current.next;
     }
-    // Return false if we make it to the end of the link list and the value is not found
+
     return false;
   }
 
-  //Returns: a string representing all the values in the Linked List, formatted as:
-  // "{ a } -> { b } -> { c } -> NULL"
   toString() {
     let returnString = "";
     let current = this.head;
