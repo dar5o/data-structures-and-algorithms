007f1288e64ccaddd4af20d0158d6cddbe18b83f
dar5o <87797682+dar5o@users.noreply.github.com>
2022-01-17 12:41:30 -0800

    Update linked-list.js

--- javascript/linked-list/linked-list.js
+++ javascript/linked-list/linked-list.js
@@ -40,6 +40,51 @@ class LinkedList {
     returnString += 'NULL';
     return returnString;
   }
+  
+  append(data) {
+    let currentNode = this.head;
+
+    while(currentNode) {
+      if(currentNode.next === null) {
+        currentNode.next = new Node(data);
+        return;
+      }
+      currentNode = currentNode.next;
+    }
+  }
+
+  insertBefore(data, newData) {
+    let currentNode = this.head;
+    let newNode = new Node(newData);
+
+    while(currentNode.data !== null) {
+      if(currentNode.data === data) {
+        newNode.next = currentNode;
+        this.head = newNode;
+      } else if(currentNode.next.data === data) {
+        let insertNode = currentNode.next;
+        currentNode.next = newNode;
+        newNode.next = insertNode;
+        return;
+      }
+      currentNode = currentNode.next;
+    }
+  }
+
+  insertAfter(data, newData) {
+    let currentNode = this.head;
+
+    while(currentNode) {
+      if(currentNode.data === data) {
+        let newNode = new Node(newData);
+        let insertNode = currentNode.next;
+        currentNode.next = newNode;
+        newNode.next = insertNode;
+      }
+      currentNode = currentNode.next;
+    }
+  }
+  
 }
 
 module.exports = LinkedList;
