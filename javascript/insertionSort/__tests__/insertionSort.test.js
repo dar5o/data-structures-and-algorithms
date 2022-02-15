0960bdb47c2b3ec48f155723d11d2983e861cd78
dar5o <87797682+dar5o@users.noreply.github.com>
2022-02-14 19:02:03 -0800

    Update insertionSort.test.js

--- javascript/insertionSort/__tests__/insertionSort.test.js
+++ javascript/insertionSort/__tests__/insertionSort.test.js
@@ -4,8 +4,8 @@ const insertionSort = require('../insertionSort.js');
 describe('testing insertionSort', () => {
 
   it('should succesfully sort an array lowest to greatest', () => {
-    let testArray = [5, 15, 40, 10, 20, 1];
-    let insertionSortTest = insertionSort(testArray);
+    let arr = [5, 15, 40, 10, 20, 1];
+    let insertionSortTest = insertionSort(arr);
 
     expect(insertionSortTest).toEqual([1, 5, 10, 15, 20, 40]);
   });
