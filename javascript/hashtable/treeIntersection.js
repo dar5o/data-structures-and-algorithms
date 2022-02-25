f99d6ada2f6d9637e7f5ffc3fd4659c49b573124
Dario Vitorte <87797682+dar5o@users.noreply.github.com>
2022-02-25 12:15:44 -0800

    Create treeIntersection.js

---
+++ javascript/hashtable/treeIntersection.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let HashTable = require('../hashTable.js');
+let tree1 = require('./tree1.js');
+let tree2 = require('./tree2.js');
+
+const treeIntersection = (tree1, tree2) => {
+
+  let table = new HashTable(512);
+  let commonValues = [];
+
+  settingTheTable(tree1.root, table);
+  
+  findCommonValues(tree2.root, table, commonValues);
+  
+  console.log(commonValues)
+  return commonValues;
+};
+
+const settingTheTable = (node, table) => {
+
+  if (!node) return;
+
+  table.set(node.value.toString());
+
+  if (node.left) settingTheTable(node.left, table);
+  if (node.right) settingTheTable(node.right, table);
+}
+
+const findCommonValues = (node, table, arr) => {
+
+  if (!node) return;
+
+  let common = table.contains(node.value.toString());
+  if (common) arr.push(node.value.toString());
+
+  if (node.left) findCommonValues(node.left, table, arr);
+  if (node.right) findCommonValues(node.right, table, arr);
+};
+
+treeIntersection(tree1, tree2);
+
+module.exports = treeIntersection;
