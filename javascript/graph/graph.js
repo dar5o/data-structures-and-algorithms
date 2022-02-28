852fbb42e3f7fa7054b51cc81d7d7d4a3d35f09e
Dario Vitorte <vitortedario@gmail.com>
2022-02-28 15:23:44 -0800

    graph challenge

---
+++ javascript/graph/graph.js
@@ -0,0 +1,91 @@
+'use strict';
+
+class Vertex {
+  constructor(value) {
+    this.value = value
+  }
+}
+
+class Edge {
+  constructor(vertex, weight) {
+    this.vertex = vertex;
+    this.weight = weight;
+  }
+}
+
+class Graph {
+  constructor() {
+    this.adjacencyList = new Map();
+  }
+
+  addVertex(value) {
+    let vertex = new Vertex(value)
+    this.adjacencyList.set(vertex, []);
+    return vertex;
+  }
+
+  addEdge(startVertex, endVertex, weight = 0) {
+    if (!this.adjacencyList.has(startVertex) || !this.adjacencyList.has(endVertex)) {
+      throw new Error('__ERROR__ Invalid Vertices');
+    }
+    const adjancancies = this.adjacencyList.get(startVertex);
+    adjancancies.push(new Edge(endVertex, weight));
+  }
+
+  getNeighbors(vertex) {
+    if (!this.adjacencyList.has(vertex)) {
+      throw new Error('__ERROR__ Invalid Vertex', vertex);
+    }
+    return [...this.adjacencyList.get(vertex)];
+  }
+
+  size() {
+    return this.adjacencyList.size;
+  }
+
+  getVertices() {
+    return this.adjacencyList;
+  }
+
+  bfs(startNode) {
+    const queue = [];
+    const visitedVertices = new Set();
+
+    queue.push(startNode);
+    visitedVertices.add(startNode);
+
+    while (queue.length) {
+      const currentNode = queue.shift();
+
+      const neighbors = this.getNeighbors(currentNode);
+
+      for (let neighbor of neighbors) {
+        const neighborNode = neighbor.vertex;
+
+        if (visitedVertices.has(neighborNode)) {
+          continue;
+        } else {
+          visitedVertices.add(neighborNode)
+        }
+        queue.push(neighborNode);
+      }
+
+    }
+    console.log('visitedNodes', visitedNodes);
+    return;
+  }
+
+
+}
+
+const graph = new Graph();
+
+const a = graph.addVertex('1');
+const b = graph.addVertex('2');
+const c = graph.addVertex('3');
+
+graph.addEdge(a, c);
+graph.addEdge(b, a);
+graph.addEdge(c, b);
+
+console.log(graph.size())
